3fb6da487227a9ffca1365df4ebc844279b0600d
Your Name <you@example.com>
2022-03-31 20:37:21 -0500

    implementacion de protocolos y rutas del router

--- Back_end/routes/router.js
+++ Back_end/routes/router.js
@@ -6,6 +6,13 @@ var projectController = require("../controller/controller");
 var router = express.Router();
 var multipart = require("connect-multiparty");
 var multipartMiddleWare = multipart({upLoadDir:"./uploads"})
-router.get("/home", projectController.home)
+
+router.post("/SaveProduct", projectController.saveProduct);
+router.get("/GetProduct/:id",projectController.getProduct);
+router.get("/GetProducts",projectController.getProducts);
+router.put("/UpdateProduct/:id",projectController.upDateProduct);
+router.delete("/DeleteProduct",projectController.deleteProduct);
+router.post("/UploadImagen/:id",multipartMiddleWare,projectController.uploadImage);
+router.get("/GetImagen/:Imagen",projectController.getImageFile);
 
 module.exports = router;
